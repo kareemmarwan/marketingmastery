49ce3ae6a638d79a63637aec22bb9b6ebb9d42da
Kareem Marwan <kareemkemoo23@gmail.com>
2026-01-01 21:20:14 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+/*import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -25,5 +25,29 @@ function App() {
     </QueryClientProvider>
   );
 }
+*/
+
+export default App;function App() {
+  return (
+    <div style={{ 
+      height: "100vh", 
+      display: "flex", 
+      justifyContent: "center", 
+      alignItems: "center",
+      fontSize: "24px"
+    }}>
+      🚧 الموقع تحت الصيانة 🚧
+    </div>
+  );
+}
 
 export default App;
+
+
+
+
+
+
+
+
+
